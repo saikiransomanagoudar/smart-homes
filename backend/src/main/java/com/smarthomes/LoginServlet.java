4858d7cab2992863433244805199be74d90b2fa3
saiki12 <saikiranhs338@gmail.com>
2024-09-15 20:03:28 -0500

    fix(login): resolve new registered user login

--- backend/src/main/java/com/smarthomes/LoginServlet.java
+++ backend/src/main/java/com/smarthomes/LoginServlet.java
@@ -44,24 +44,21 @@ public class LoginServlet extends HttpServlet {
                 hm = (HashMap<String, User>) objectInputStream.readObject();
                 objectInputStream.close();
                 fileInputStream.close();
+
+                // Debugging: Print all users in the system
+                System.out.println("Users in system: ");
+                hm.values().forEach(user -> System.out.println(user.email()));
             }
 
-            // Check if email or email exists and password matches
-            boolean validCredentials = false;
-            User loggedInUser = null;
-
-            for (User user : hm.values()) {
-                // Check if email or email matches and if the password is correct
-                if (user.email().equals(email) && user.password().equals(password)) {
-                    validCredentials = true;
-                    loggedInUser = user;
-                    break;
-                }
-            }
-
-            if (validCredentials) {
+            // Check if email exists and password matches
+            User loggedInUser = hm.get(email);
+            
+            // Check if user exists and password matches
+            if (loggedInUser != null && loggedInUser.password().equals(password)) {
                 response.setStatus(HttpServletResponse.SC_OK);
                 out.print("{ \"message\": \"Login successful\", \"email\": \"" + loggedInUser.email() + "\" }");
+
+                // Set user in session
                 HttpSession session = request.getSession(true);
                 session.setAttribute("username", loggedInUser.email());
                 System.out.println("Username set in session: " + session.getAttribute("username"));
@@ -69,7 +66,7 @@ public class LoginServlet extends HttpServlet {
                 // Incorrect email or password
                 error_msg = "Invalid email or password!";
             }
-            
+
         } catch (Exception e) {
             e.printStackTrace();
             error_msg = "An error occurred during login!";
@@ -81,6 +78,6 @@ public class LoginServlet extends HttpServlet {
             out.print("{ \"error\": \"" + error_msg + "\" }");
         }
 
-        out.flush(); // Ensure the response body is written to the client
+        out.flush(); 
     }
 }
